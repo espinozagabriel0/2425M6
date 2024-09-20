0eebc19e1207b21f23581b81e9e6ff620f537213
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-20 16:32:33 +0000

    initial commit

---
+++ sesiones/sesion3/script.js
@@ -0,0 +1,7 @@
+const boton1 = document.querySelector('#btn1')
+const boton2 = document.querySelector('#btn2')
+
+
+console.log(boton1)
+console.log(boton2)
+console.log(document.querySelector('h1').innerHTML = "<em>nuevo titulo</em>")
