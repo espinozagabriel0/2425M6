5f00e303cac8d6c95f79978fde5a14ebea912ec8
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-22 15:53:00 +0000

    script afegit

--- ejerciciosUF1/ejercicio2-calc/script.js
+++ ejerciciosUF1/ejercicio2-calc/script.js
@@ -11,50 +11,95 @@ const boton8 = document.querySelector('#btn8')
 const boton9 = document.querySelector('#btn9')
 const boton0 = document.querySelector('#btn0')
 const btnBorrar = document.querySelector('#btnBorrar')
-
+const btnRestar = document.querySelector('#btnResta')
+const btnSumar =  document.querySelector('#btnSuma')
+const btnIgual = document.querySelector('#btnIgual')
 
 const pantalla = document.querySelector('#pantalla')
+let entradaActual = ''
 
-boton1.addEventListener('click', function name(params) {
-    pantalla.innerHTML = "1"
+boton1.addEventListener('click', function () {
+    entradaActual += '1'
+    pantalla.innerHTML = entradaActual
 })
 
 boton2.addEventListener('click', function() {
-    pantalla.innerHTML = "2"
+    entradaActual += '2'
+    pantalla.innerHTML = entradaActual
 })
 
 boton3.addEventListener('click', function() {
-    pantalla.innerHTML = "3"
+    entradaActual += '3'
+    pantalla.innerHTML = entradaActual
 })
 
 boton4.addEventListener('click', function() {
-    pantalla.innerHTML = "4"
+    entradaActual += '4'
+    pantalla.innerHTML = entradaActual
 })
 
 boton5.addEventListener('click', function() {
-    pantalla.innerHTML = "5"
+    entradaActual += '5'
+    pantalla.innerHTML = entradaActual
 })
 
 boton6.addEventListener('click', function() {
-    pantalla.innerHTML = "6"
+    entradaActual += '6'
+    pantalla.innerHTML = entradaActual
 })
 
 boton7.addEventListener('click', function() {
-    pantalla.innerHTML = "7"
+    entradaActual += '7'
+    pantalla.innerHTML = entradaActual
 })
 
 boton8.addEventListener('click', function() {
-    pantalla.innerHTML = "8"
+    entradaActual += '8'
+    pantalla.innerHTML = entradaActual
 })
 
 boton9.addEventListener('click', function() {
-    pantalla.innerHTML = "9"
+    entradaActual += '9'
+    pantalla.innerHTML = entradaActual
 })
 
 boton0.addEventListener('click', function() {
-    pantalla.innerHTML = "0"
+    cont = 0
+    if (parseInt(pantalla.innerHTML) == 0) {
+        entradaActual = '0'
+        pantalla.innerHTML = entradaActual
+    }else{
+        entradaActual += '0'
+        pantalla.innerHTML = entradaActual
+    }
 })
 
+btnSumar.addEventListener('click', function() {
+    entradaActual += '+'
+    pantalla.innerHTML = entradaActual
+})
+btnRestar.addEventListener('click', function() {
+    entradaActual += '-'
+    pantalla.innerHTML = entradaActual
+})
+btnIgual.addEventListener('click', function() {
+    try {
+        if (pantalla.innerHTML != "0") {
+            let resultado = eval(entradaActual)
+            pantalla.innerHTML = eval(resultado)
+            entradaActual = resultado
+            console.log("Resultado: ", resultado)
+        }else{
+            throw "Nombre no valid"
+        }
+    } catch (error) {
+        pantalla.innerHTML = 'Error'
+        console.log("Error: ", error)
+    }
+})
+
+
 btnBorrar.addEventListener('click', function() {
-    pantalla.innerHTML = "0"
+    entradaActual = ''
+    pantalla.innerHTML = entradaActual + '0'
 })
