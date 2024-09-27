8fd946c15b5ac9d0074911e08525f31e69b4b1db
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-27 15:51:42 +0000

    ejercicio5-ficha

--- ejerciciosUF1/ejercicio5-fitxa/main.js
+++ ejerciciosUF1/ejercicio5-fitxa/main.js
@@ -1,24 +1,29 @@
 console.log("hello world")
 
 const btnEnviarFicha = document.querySelector('button.btn')
-const nombreInput = document.querySelector('input.name')
+const nombreInput = document.querySelector('input#name')
 const ap1Input = document.querySelector('input#ap1')
 const ap2Input = document.querySelector('input#ap2')
 const dniInput = document.querySelector('input#dni')
 const urlInput = document.querySelector('input#url')
 
+const nombreFicha  = document.querySelector('span#nomFicha')
+const apellidosFicha = document.querySelector('span#apellidosFicha')
+const dniFicha = document.querySelector('span#dniFicha')
 
-const nom = nombreInput.value
-const ap1 = ap1Input.value
-const ap2 = ap2Input.value
-const dni = dniInputInput.value
-const url = urlInput.value
+let nom = nombreInput.value
+let ap1 = ap1Input.value
+let ap2 = ap2Input.value
+let dni = dniInput.value
+let url = urlInput.value
 
 function agruparDades() {
-    
+    return ap1 + ap2
 }
 
 //FUNCION QUE CREA LA FICHA
-btnenviarFicha.addEventListener('click', function() {
-    
+btnEnviarFicha.addEventListener('click', function() {
+    nombreFicha.innerHTML = nom
+    apellidosFicha.innerHTML = agruparDades()
+    dniFicha.innerHTML = dni
 })
