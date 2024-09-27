5a0c78021938563747601e4fdb86b55d1e63b7a1
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-27 15:40:52 +0000

    ejercicio5-ficha

--- ejerciciosUF1/ejercicio5-fitxa/main.js
+++ ejerciciosUF1/ejercicio5-fitxa/main.js
@@ -8,6 +8,15 @@ const dniInput = document.querySelector('input#dni')
 const urlInput = document.querySelector('input#url')
 
 
+const nom = nombreInput.value
+const ap1 = ap1Input.value
+const ap2 = ap2Input.value
+const dni = dniInputInput.value
+const url = urlInput.value
+
+function agruparDades() {
+    
+}
 
 //FUNCION QUE CREA LA FICHA
 btnenviarFicha.addEventListener('click', function() {
