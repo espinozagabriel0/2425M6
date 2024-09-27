e767246628d0e2558ac8228712e09e5acc58f7c8
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-27 15:30:47 +0000

    ejercicio5-ficha

---
+++ ejerciciosUF1/ejercicio5-fitxa/main.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+
+const btnEnviarFicha = document.querySelector('button.btn')
+const nombreInput = document.querySelector('input.name')
+const ap1Input = document.querySelector('input#ap1')
+const ap2Input = document.querySelector('input#ap2')
+const dniInput = document.querySelector('input#dni')
+const urlInput = document.querySelector('input#url')
+
+
+
+//FUNCION QUE CREA LA FICHA
+btnenviarFicha.addEventListener('click', function() {
+    
+})
