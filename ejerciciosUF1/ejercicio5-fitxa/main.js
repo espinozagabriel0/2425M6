c6730749ab82998aa3b898c971e489fc624d37fc
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-30 09:58:07 +0000

    ej5-fitxa validación URL

--- ejerciciosUF1/ejercicio5-fitxa/main.js
+++ ejerciciosUF1/ejercicio5-fitxa/main.js
@@ -1,5 +1,3 @@
-// console.log("hello world")
-
 const btnEnviarFicha = document.querySelector('button.btn')
 const nombreInput = document.querySelector('input#name')
 const ap1Input = document.querySelector('input#ap1')
@@ -24,27 +22,28 @@ btnEnviarFicha.addEventListener('click', function(e) {
         nombreFicha.innerHTML = "John"
     }
 
-    if (ap1Input.value != "" || "" != ap2Input.value) {
+    if (ap1Input.value != "" && "" != ap2Input.value) {
         apellidosFicha.innerHTML = agruparApellidos()
     }else{
         console.log("Introduce los dos apellidos")
-        apellidosFicha.innerHTML = "Doe" 
+        apellidosFicha.innerHTML = " Doe" 
     }
 
-    apellidosFicha.innerHTML = agruparApellidos()
-    dniFicha.innerHTML = dniInput.value
+    // Validación DNI
+    if (dniInput.value != "") {
+        dniFicha.innerHTML = dniInput.value
+    } else {
+        dniFicha.innerHTML = '00000000M'
+    }
 
-    //Control errores
-    try {
-        if (urlInput.value == "") {
-            throw "Error: Introduce una URL de imagen válida"
-        }
-    
+    //Validación URL imagen
+    if (esValidoUrl(urlInput.value)) {
         imgFicha.src = `${urlInput.value}`
-    } catch (error) {
-        console.log(error)
+    }else{
+        imgFicha.src = "https://www.researchgate.net/profile/Maria-Monreal/publication/315108532/figure/fig1/AS:472492935520261@1489662502634/Figura-2-Avatar-que-aparece-por-defecto-en-Facebook.png"
     }
 
+    
     console.log(nombreFicha.innerHTML)
     console.log(apellidosFicha.innerHTML)
     console.log(dniFicha.innerHTML)
@@ -52,14 +51,25 @@ btnEnviarFicha.addEventListener('click', function(e) {
     limpiarFormulario()
 })
 
+//Función que agrupa los apellidos 
 function agruparApellidos(){
     return ` ${ap1Input.value} ${ap2Input.value}`
 }
+//Función que válida si la URL proporcionada es válida o no
+function esValidoUrl(urlString) {
+    try {
+        let url =  new URL(urlString);
+        console.log(url);
+        return true;
+    } catch (error) {
+        console.log("Error: URL no válida");
+        return false;
+    }
+}
 function limpiarFormulario(){
-
-    nombreInput.value = ""
-    ap1Input.value = ""
-    ap2Input.value = ""
-    dniInput.value = ""
-    urlInput.value = ""
+    nombreInput.value = "";
+    ap1Input.value = "";
+    ap2Input.value = "";
+    dniInput.value = "";
+    urlInput.value = "";
 }
