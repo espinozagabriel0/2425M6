68dbeb5b2a5a90cf0156fda75b3b42f9f2a921a4
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-09-30 10:56:14 +0000

    ej5-fitxa fet

--- ejerciciosUF1/ejercicio5-fitxa/main.js
+++ ejerciciosUF1/ejercicio5-fitxa/main.js
@@ -11,11 +11,10 @@ const dniFicha = document.querySelector('span#dniFicha')
 
 // Imagen ficha
 const imgFicha = document.querySelector('img.card-img-top')
+const inputs = document.querySelectorAll('input')
 
-//FUNCION QUE CREA LA FICHA
+//Función que crea la ficha del usuario
 btnEnviarFicha.addEventListener('click', function(e) {
-    e.preventDefault()
-
     if (nombreInput.value != "") {
         nombreFicha.innerHTML = nombreInput.value
     }else{
@@ -43,13 +42,34 @@ btnEnviarFicha.addEventListener('click', function(e) {
         imgFicha.src = "https://www.researchgate.net/profile/Maria-Monreal/publication/315108532/figure/fig1/AS:472492935520261@1489662502634/Figura-2-Avatar-que-aparece-por-defecto-en-Facebook.png"
     }
 
-    
     console.log(nombreFicha.innerHTML)
     console.log(apellidosFicha.innerHTML)
     console.log(dniFicha.innerHTML)
 
     limpiarFormulario()
 })
+inputs.forEach(input => {
+    input.addEventListener('input', function() {
+        if (input.id != 'url') {
+            if (input.value.trim() == "") {
+                input.classList.add('is-invalid')
+                input.classList.remove('is-valid')
+            }else{
+                input.classList.add('is-valid')
+                input.classList.remove('is-invalid')
+            }
+        } else {
+            if (esValidoUrl(input.value)) {
+                input.classList.add('is-valid')
+                input.classList.remove('is-invalid')
+            }else{
+                console.log(input)
+                input.classList.add('is-invalid')
+                input.classList.remove('is-valid')
+            }
+        }
+    })
+});
 
 //Función que agrupa los apellidos 
 function agruparApellidos(){
@@ -72,4 +92,11 @@ function limpiarFormulario(){
     ap2Input.value = "";
     dniInput.value = "";
     urlInput.value = "";
+
+    //volver a eliminar las clases de las validaciones de los inputs
+    inputs.forEach(input =>{
+        input.classList.remove('is-valid')
+        input.classList.remove('is-invalid')
+    })
 }
+
