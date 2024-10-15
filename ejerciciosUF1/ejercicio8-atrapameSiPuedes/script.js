3d684b83563acbc6e4b841d172230b4de7449637
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-10-15 18:25:39 +0000

    ej 8 tablero funcionamiento básico movimiento fichas

--- ejerciciosUF1/ejercicio8-atrapameSiPuedes/script.js
+++ ejerciciosUF1/ejercicio8-atrapameSiPuedes/script.js
@@ -21,6 +21,12 @@ const ficha2 = document.querySelector('.ficha2');
 
 const reload = document.querySelector('button#reload');
 
+
+
+//dados jugadores (simplemente para visualizar que numero se genera al tirar al dado)
+const txtDado1 = document.querySelector('span#dado1Mostrar');
+const txtDado2 = document.querySelector('span#dado2Mostrar');
+
 //variables globales
 let posActualFicha1 = 0;
 let posActualFicha2 = 0;
@@ -30,24 +36,24 @@ const posiciones = [
     {x: "177", y: "440"},
     {x: "196", y: "363"},
     {x: "213", y: "298"},
-    {x: "222", y: "225"},
+    {x: "222", y: "225"}, // --> posicion 5: se pierde un turno
     {x: "230", y: "150"}, // 1, 6
-    {x: "314", y: "145"},
+    {x: "314", y: "145"}, // --> posicion 7: avanzar a la siguiente estrella
     {x: "395", y: "145"},
     {x: "466", y: "145"},
     {x: "536", y: "145"},
-    {x: "613", y: "145"},
-    {x: "693", y: "145"}, // 6, 12
+    {x: "613", y: "145"}, // --> posicion 11: estrella
+    {x: "693", y: "145"}, // 6, 12 --> pos 12: volver a casilla 0
     {x: "695", y: "226"},
-    {x: "711", y: "308"},
+    {x: "711", y: "308"}, // --> pos 14: ir a casilla 29
     {x: "724", y: "399"},
     {x: "730", y: "496"}, // 12, 16
     {x: "625", y: "496"},
-    {x: "516", y: "496"},
+    {x: "516", y: "496"}, // --> pos 18: se pierde 1 turno
     {x: "418", y: "496"},  
     {x: "318", y: "496"}, // 16, 20
     {x: "288", y: "428"},
-    {x: "295", y: "337"},
+    {x: "295", y: "337"}, 
     {x: "300", y: "227"},
     {x: "380", y: "227"}, // 20, 23
     {x: "452", y: "227"},
@@ -55,7 +61,7 @@ const posiciones = [
     {x: "610", y: "227"},
     {x: "610", y: "286"}, // 23, 27
     {x: "610", y: "341"},
-    {x: "610", y: "405"},
+    {x: "610", y: "405"}, // pos 30 (se puede llegar desde pos 14)
     {x: "534", y: "405"}, // 27, 30
     {x: "452", y: "405"},
     {x: "380", y: "405"},
@@ -85,44 +91,64 @@ function start(){
     ficha1.style.left = posiciones[0].x + 'px';
     ficha2.style.top = posiciones[0].y + 'px';
     ficha2.style.left = posiciones[0].x + 'px';
+    posActualFicha1 = 0;
+    posActualFicha2 = 0;
 }
 
+//DADO 1er JUGADOR
 dado1.addEventListener('click', function() {
-    // moverFicha(ficha1);
-
     //TODO: GENERAR NUMERO ALEATORIO Y MOVER TANTAS POSICIONES COMO EL NUMERO DEL DADO INDIQUE
     let numPosiciones = Math.floor(Math.random() * 6) + 1;
-    console.log(numPosiciones);
+    console.log('numero generado J1: ', numPosiciones);
+    txtDado1.innerHTML = numPosiciones;
 
-    moverFicha(ficha1, numPosiciones);
+    moverFicha(ficha1, numPosiciones, posActualFicha1);
 
 });
 
-function moverFicha(ficha, numPosiciones) {
+// DADO 2o JUGADOR
+dado2.addEventListener('click', function() {
+    //TODO: GENERAR NUMERO ALEATORIO Y MOVER TANTAS POSICIONES COMO EL NUMERO DEL DADO INDIQUE
+    let numPosiciones = Math.floor(Math.random() * 6) + 1;
+    console.log('numero generado J2: ', numPosiciones);
+    txtDado2.innerHTML = numPosiciones;
+
+    moverFicha(ficha2, numPosiciones, posActualFicha2);
+
+});
+
+function moverFicha(ficha, numPosiciones, posActual) {
     console.log('posiciones a mover: ', numPosiciones);
-    
-    function mover() {
-        if (posicionActual < numPosiciones) {
+    console.log("---------------------")
+  
+    let i = posActual;
+    let casillaFinal = numPosiciones + posActual;
+
+    if (numPosiciones + posActual > 36) {
+        console.log("te has pasado!")
+    }
+
+    if (ficha == ficha1) {
+        posActualFicha1 += numPosiciones    
+    }else{
+        posActualFicha2 += numPosiciones;
+    }
+
+    function mover() {        
+        if (i <= casillaFinal) {
             ficha.style.top = posiciones[i].y + 'px';
             ficha.style.left = posiciones[i].x + 'px';
+            
             i++;
-
-            if (ficha == ficha1) {
-                posActualFicha1++;
-            }else{
-                posActualFicha2++;
-            }
-
             setTimeout(mover, 300);
         }
     }
+    console.log('pos actual J1: ', posActualFicha1 + 1);
+    console.log('pos actual J2: ', posActualFicha2 + 1);    
+
+    // if (posActualFicha1 == posActualFicha2) {
+        
+    // }
     
     mover();
 }
-
-
-
-dado2.addEventListener('click', function() {
-    ficha2.style.top = '';
-    ficha2.style.left = '';
-});
