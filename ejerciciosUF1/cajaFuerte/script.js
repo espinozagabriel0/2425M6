8c5231c966c45b95ab40e0676b05d51b2df335f8
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2024-10-18 15:48:42 +0000

    comienzo de caja fuerte virtual ejercicios

---
+++ ejerciciosUF1/cajaFuerte/script.js
@@ -0,0 +1,16 @@
+const boton1 = document.querySelector('button#boton1');
+const boton2 = document.querySelector('button#boton2');
+const boton3 = document.querySelector('button#boton3');
+
+// EJ 1
+boton1.addEventListener('click', function() {
+    console.log("Hola!")
+});
+boton2.addEventListener('click', function() {
+    console.log("Muy buenas!")
+});
+boton3.addEventListener('click', function() {
+    console.log("Noo!")
+});
+
+// EJ 2
